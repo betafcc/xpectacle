85c0c7baeabd3a1e8e671d0058c12704ce67bb2e
Beta Faccion <betafcc@gmail.com>
2018-06-20 19:35:18 -0300

    Adds map method

--- xpectacle/active_window.py
+++ xpectacle/active_window.py
@@ -10,3 +10,8 @@ class ActiveWindow:
 
     def apply(self) -> None:
         core.transform(self.transform(core.viewport(), core.window()))
+
+    def map(self,
+            f : Callable[[core.Geometry, core.Geometry], core.Geometry],
+            ) -> 'ActiveWindow':
+        return replace(self, transform=lambda vp, win: self.transform(vp, f(vp, win)))
